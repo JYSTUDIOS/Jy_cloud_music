ee45c004fa4046f9e2116f19e3396922c6f59405
Jy <jjyy20001515@outlook.com>
2018-08-06 13:57:19 +0800

    update

--- js/router.js
+++ js/router.js
@@ -1,26 +1,95 @@
 $(function(){
 
     //切换
-    $(".bottom1").click(function() {
+    $("#bot1").click(function() {
 
-        $(".body").load($(this).data("page1"))
+        $(".body").load($(this).data("page"))
+        $("#bot1").children("img").attr("src",$("#bot1").data("normal1"))
+        $("#bot2").children("img").attr("src",$("#bot2").data("normal2"))
+        $("#bot3").children("img").attr("src",$("#bot3").data("normal3"))
+        $("#bot4").children("img").attr("src",$("#bot4").data("normal4"))
+        $("#bot5").children("img").attr("src",$("#bot5").data("normal5"))
 
+        $(this).children("img").attr("src",$(this).data("selected1"))
+
+        $("#bof1").attr("style",$("#bof1").data("selectedfont1"))
+        $("#bof2").attr("style",$("#bof2").data("normalfont2"))
+        $("#bof3").attr("style",$("#bof3").data("normalfont3"))
+        $("#bof4").attr("style",$("#bof4").data("normalfont4"))
+        $("#bof5").attr("style",$("#bof5").data("normalfont5"))
+    })
+    $("#bot2").click(function() {
+
+        $(".body").load($(this).data("page"))
+        $("#bot1").children("img").attr("src",$("#bot1").data("normal1"))
+        $("#bot2").children("img").attr("src",$("#bot2").data("normal2"))
+        $("#bot3").children("img").attr("src",$("#bot3").data("normal3"))
+        $("#bot4").children("img").attr("src",$("#bot4").data("normal4"))
+        $("#bot5").children("img").attr("src",$("#bot5").data("normal5"))
+        $(this).children("img").attr("src",$(this).data("selected2"))
+        $("#bof1").attr("style",$("#bof1").data("normalfont1"))
+        $("#bof2").attr("style",$("#bof2").data("selectedfont2"))
+        $("#bof3").attr("style",$("#bof3").data("normalfont3"))
+        $("#bof4").attr("style",$("#bof4").data("normalfont4"))
+        $("#bof5").attr("style",$("#bof5").data("normalfont5"))
+    })
+    $("#bot3").click(function() {
+
+        $(".body").load($(this).data("page"))
+        $("#bot1").children("img").attr("src",$("#bot1").data("normal1"))
+        $("#bot2").children("img").attr("src",$("#bot2").data("normal2"))
+        $("#bot3").children("img").attr("src",$("#bot3").data("normal3"))
+        $("#bot4").children("img").attr("src",$("#bot4").data("normal4"))
+        $("#bot5").children("img").attr("src",$("#bot5").data("normal5"))
+        $(this).children("img").attr("src",$(this).data("selected3"))
+
+        $("#bof1").attr("style",$("#bof1").data("normalfont1"))
+        $("#bof2").attr("style",$("#bof2").data("normalfont2"))
+        $("#bof3").attr("style",$("#bof3").data("selectedfont3"))
+        $("#bof4").attr("style",$("#bof4").data("normalfont4"))
+        $("#bof5").attr("style",$("#bof5").data("normalfont5"))
+    })
+    $("#bot4").click(function() {
+
+        $(".body").load($(this).data("page"))
+        $("#bot1").children("img").attr("src",$("#bot1").data("normal1"))
+        $("#bot2").children("img").attr("src",$("#bot2").data("normal2"))
+        $("#bot3").children("img").attr("src",$("#bot3").data("normal3"))
+        $("#bot4").children("img").attr("src",$("#bot4").data("normal4"))
+        $("#bot5").children("img").attr("src",$("#bot5").data("normal5"))
+        $(this).children("img").attr("src",$(this).data("selected4"))
+
+        $("#bof1").attr("style",$("#bof1").data("normalfont1"))
+        $("#bof2").attr("style",$("#bof2").data("normalfont2"))
+        $("#bof3").attr("style",$("#bof3").data("normalfont3"))
+        $("#bof4").attr("style",$("#bof4").data("selectedfont4"))
+        $("#bof5").attr("style",$("#bof5").data("normalfont5"))
+    })
+    $("#bot5").click(function() {
+
+        $(".body").load($(this).data("page"))
+        $("#bot1").children("img").attr("src",$("#bot1").data("normal1"))
+        $("#bot2").children("img").attr("src",$("#bot2").data("normal2"))
+        $("#bot3").children("img").attr("src",$("#bot3").data("normal3"))
+        $("#bot4").children("img").attr("src",$("#bot4").data("normal4"))
+        $("#bot5").children("img").attr("src",$("#bot5").data("normal5"))
+        $(this).children("img").attr("src",$(this).data("selected5"))
+
+        $("#bof1").attr("style",$("#bof1").data("normalfont1"))
+        $("#bof2").attr("style",$("#bof2").data("normalfont2"))
+        $("#bof3").attr("style",$("#bof3").data("normalfont3"))
+        $("#bof4").attr("style",$("#bof4").data("normalfont4"))
+        $("#bof5").attr("style",$("#bof5").data("selectedfont5"))
     })
 
-    $(".bottom3").click(function() {
+    $(".small").click(function() {
 
-        $(".body").load($(this).data("page3"))
-
-    })
-
-    $(".bottom4").click(function() {
-
-        $(".body").load($(this).data("page4"))
+        window.history.go(-1);
 
     })
-    $(".bottom5").click(function() {
+    $(".listening").click(function() {
 
-        $(".body").load($(this).data("page5"))
+        $(".body").load($(this).data("page"))
 
     })
 
